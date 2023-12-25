d12560a095eacc836b7e7a0636d32b59b179895a
Helkayam <henelkayam99@gmail.com>
2023-12-25 17:38:11 +0200

    Initial Code + Welcome Functions

---
+++ Stage0/Program3602.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome3602();
+            Welcome2068();
+            Console.ReadKey();
+
+
+        }
+        static partial void Welcome2068();
+        
+
+        private static void Welcome3602()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+
+    }
+}
