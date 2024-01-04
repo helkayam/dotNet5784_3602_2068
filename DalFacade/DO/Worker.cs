361df9c48c0bc4e65c651284bada7b0302c9c5fc
LeaSimBer <lea.s.berdugo@gmail.com>
2024-01-04 22:32:14 +0200

    I added documentation of Worker Entity.

--- DalFacade/DO/Worker.cs
+++ DalFacade/DO/Worker.cs
@@ -1,5 +1,13 @@
-﻿namespace DO;
-
+﻿
+namespace DO;
+/// <summary>
+/// Workers entityrepresent a worker with all its props
+/// </summary>
+/// <param name="Id">ID of the worker >
+/// <param name="Level">Professional Level of the worker>
+/// <param name="Name">First Name and Last name of the worker >
+/// <param name="PhoneNumber">Phone number of the workers>
+/// <param name="Cost">salary per hour of the worker depending on his level>
 public record Worker
 (
     int Id,
@@ -10,7 +18,7 @@ double? Cost=null
 
 )
 {
-    public Worker(): this(0,0," ") { }
+    public Worker(): this(0,0," ") { }//empty constructor of Worker
    
 
 }
