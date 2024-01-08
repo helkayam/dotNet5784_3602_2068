873f4efb83f1bec3ed1ea236df9cc798d9a0cfc7
LeaSimBer <lea.s.berdugo@gmail.com>
2024-01-08 14:18:20 +0200

    I added a generic interface ICrud, and I changed the interfaces in DalApi inherit from the ICrud interface and i added a new  IDal interface

---
+++ DalFacade/DalApi/ICRUD.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace DalApi
+{
+    public interface ICrud<T> where T:class
+    {
+        /// <summary>
+        /// Creates new entity object in DAL
+        /// </summary>
+        /// <param name="item">entity object</param>
+        /// <returns></returns>
+        int Create(T item);
+
+        /// <summary>
+        /// Reads entity object by its ID 
+        /// </summary>
+        /// <param name="id">id of entity</param>
+        /// <returns></returns>
+        T? Read(int id);
+
+        /// <summary>
+        /// Reads all entity objects
+        /// </summary>
+        /// <returns></returns>
+        List<T> ReadAll();
+
+        /// <summary>
+        /// Updates entity object
+        /// </summary>
+        /// <param name="item">entity object</param>
+        void Update(T item);
+
+        /// <summary>
+        /// Deletes an object by its Id
+        /// </summary>
+        /// <param name="id">ID of the object</param>
+        void Delete(int id);
+    }
+}
---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface IDal
+    {
+        IDependency Dependency { get; }
+        IWorker Worker { get; }
+        ITask task { get; }
+
+
+    }
+}
