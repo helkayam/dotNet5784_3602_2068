fc45513626993810e556d64820b5963196bda855
LeaSimBer <lea.s.berdugo@gmail.com>
2024-01-15 14:40:00 +0200

    I created a new project named DalXml. I created configuration file and file that will be the data base (all in xml library). And I created config class for configuration data.

---
+++ DalXml/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+   
+}
