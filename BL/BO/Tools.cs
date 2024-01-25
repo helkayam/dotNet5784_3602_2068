d47ab6230cd2e2bf28faa5c4add1a18f014a9fd1
LeaSimBer <lea.s.berdugo@gmail.com>
2024-01-25 23:59:29 +0200

    I added the static class Tools for helper method.

---
+++ BL/BO/Tools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+
+static internal class Tools
+{
+    //public static string ToStringProperty<T>(this T item)
+    //{
+    //    Type type= typeof(T);
+    //    StringBuilder sb = new StringBuilder();
+    //    foreach( var prop in type.GetProperties ())
+    //    {
+    //        sb.Append(prop.Name);
+    //        sb.Append(": ");
+
+    //        if(Index)
+    //        sb.Append(item.GetType().GetProperty(prop.Name).Name);
+    //        sb.Append(System.Environment.NewLine);
+    //    }
+    //}
+}
